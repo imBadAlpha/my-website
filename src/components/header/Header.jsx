2e34f805abf611ab47e1502f8d9d53c9963a6374
IamBadAlpha <romeonombrejr@gmail.com>
2023-03-18 14:12:25 +0800

    CSS styles and Variables initialized

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,13 @@ import './header.css'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className='container header__container'>
+        <h5>Hey there, I'm</h5>
+        <h1>Romeo Nombre Jr.</h1>
+        <h5 className='text-light'>Fullstack Developer</h5>
+      </div>
+    </header>
   )
 }
 
