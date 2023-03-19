ee5618ee5f35c9825b68b6c81b5d31ae883b366f
IamBadAlpha <romeonombrejr@gmail.com>
2023-03-19 22:41:37 +0800

    Finished the Nav Component

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -4,9 +4,9 @@ import {BsGithub, BsLinkedin, BsFacebook} from 'react-icons/bs'
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href=""><BsGithub /></a>
-        <a href=""><BsLinkedin /></a>
-        <a href=""><BsFacebook /></a>
+        <a href="https://github.com/imBadAlpha" title='Github'><BsGithub /></a>
+        <a href="https://www.linkedin.com/in/romeo-nombre-jr-a32835217/" title='LinkedIn'><BsLinkedin /></a>
+        <a href="https://www.facebook.com/romesh.junior.93/" title='Facebook'><BsFacebook /></a>
     </div>
   )
 }
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import './nav.css'
+import { AiOutlineHome, AiOutlineUser, AiOutlineProject, } from 'react-icons/ai'
+import { BiBook } from 'react-icons/bi'
+import { MdDesignServices, MdOutlineContactSupport } from 'react-icons/md'
+import { useState } from 'react'
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#" title='Home' onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
+      <a href="#about" title='About' onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser /></a>
+      <a href="#experience" title='Experience' onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBook /></a>
+      <a href="#services" title='Services' onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><MdDesignServices /></a>
+      <a href="#portfolio" title='Portfolio' onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><AiOutlineProject /></a>
+      <a href="#contact" title='Contact' onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><MdOutlineContactSupport /></a>
+    </nav>
   )
 }
 
