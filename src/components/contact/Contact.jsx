a866ff0ea54f6acfaa56497001cdb88d8e1f29da
IamBadAlpha <romeonombrejr@gmail.com>
2023-03-27 16:30:27 +0800

    Finished Contact component

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
 import './contact.css'
+import { MdOutlineMailOutline } from 'react-icons/md'
+import { RiMessengerLine } from 'react-icons/ri'
+import { AiOutlineLinkedin } from 'react-icons/ai'
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className='container contact__container'>
+        <div className='contact__options'>
+          <article className='contact__option'>
+            <MdOutlineMailOutline className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>romeonombrejr@gmail.com</h5>
+            <a href="mailto:romeonombrejr@gmail.com" target='_blank'>Send a message</a>
+          </article>
+
+          <article className='contact__option'>
+            <RiMessengerLine className='contact__option-icon'/>
+            <h4>Messenger</h4>
+            <h5>Romeo Nombre Jr.</h5>
+            <a href="https://m.me/romesh.junior.93" target='_blank'>Send a message</a>
+          </article>
+
+          <article className='contact__option'>
+            <AiOutlineLinkedin className='contact__option-icon'/>
+            <h4>LinkedIn</h4>
+            <h5>Romeo Nombre Jr.</h5>
+            <a href="https://www.linkedin.com/in/romeo-nombre-jr-a32835217" target='_blank'>Send a message</a>
+          </article>
+        </div>
+        <form action="">
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="email" name='email' placeholder='Your Email' required />
+          <textarea name="message" id="message" rows="7" placeholder='Your message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -2,11 +2,35 @@ import React from 'react'
 import './nav.css'
 import { AiOutlineHome, AiOutlineUser, AiOutlineProject, } from 'react-icons/ai'
 import { BiBook } from 'react-icons/bi'
-import { MdDesignServices, MdOutlineContactSupport } from 'react-icons/md'
-import { useState } from 'react'
+import { MdDesignServices, MdOutlineContactSupport, MdPreview } from 'react-icons/md'
+import { useState, useEffect} from 'react'
 
 const Nav = () => {
-  const [activeNav, setActiveNav] = useState('#')
+  const [activeNav, setActiveNav] = useState('#');
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const sections = document.querySelectorAll('section');
+      const scrollPosition = window.pageYOffset;
+
+      sections.forEach((section) => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+
+        if (scrollPosition >= sectionTop - sectionHeight / 3) {
+          const sectionId = section.getAttribute('id');
+          setActiveNav(`#${sectionId}`);
+        }
+      });
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <nav>
       <a href="#" title='Home' onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome /></a>
@@ -14,6 +38,7 @@ const Nav = () => {
       <a href="#experience" title='Experience' onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBook /></a>
       <a href="#services" title='Services' onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><MdDesignServices /></a>
       <a href="#portfolio" title='Portfolio' onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><AiOutlineProject /></a>
+      <a href="#testimonials" title='Testimonials' onClick={() => setActiveNav('#Testimonials')} className={activeNav === '#testimonials' ? 'active' : ''}><MdPreview /></a>
       <a href="#contact" title='Contact' onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><MdOutlineContactSupport /></a>
     </nav>
   )
